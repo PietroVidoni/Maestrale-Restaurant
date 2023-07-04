3c4001eef03709161ef2ad4b9dfc0d47a906d3df
PietroVidoni <pietro.vidoni04@gmail.com>
2023-07-04 22:19:11 +0200

    Navbar and Hero-section dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 /* 
     Script.css
 */
+
+function toggleShape(link) {
+    // Rimuovi la classe 'active' da tutti i link
+    var links = document.querySelectorAll('.nav-link');
+    links.forEach(function(link) {
+      link.classList.remove('active');
+    });
+  
+    // Aggiungi la classe 'active' al link cliccato
+    link.classList.add('active');
+  }
+  
