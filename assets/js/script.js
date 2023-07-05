e1af3b697d280008f5a3717bce30dc5a3a184563
Emir-Brkic <81163301+Emir-Brkic@users.noreply.github.com>
2023-07-05 15:27:21 +0200

    New branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,1 @@
-/* 
-    Script.css
-*/
 
-const preloader = document.querySelector("[data-preaload]");
-
-window.addEventListener("load", function () {
-    preloader.classList.add("loaded");
-    document.body.classList.add("loaded");
-});
