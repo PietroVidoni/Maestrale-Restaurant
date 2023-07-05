eeaa46a287e288546c4b2d04709cddc6fa150565
PietroVidoni <pietro.vidoni04@gmail.com>
2023-07-05 11:14:41 +0200

    First navbar and animations implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,16 @@
 */
 
 function toggleShape(link) {
-    // Rimuovi la classe 'active' da tutti i link
-    var links = document.querySelectorAll('.nav-link');
-    links.forEach(function(link) {
-      link.classList.remove('active');
-    });
-  
-    // Aggiungi la classe 'active' al link cliccato
-    link.classList.add('active');
-  }
+  var links = document.querySelectorAll('.nav-link');
+  links.forEach(function (link) {
+      link.parentNode.classList.remove('active');
+      link.parentNode.classList.remove('svg-background');
+  });
+
+  link.parentNode.classList.add('active');
+  link.parentNode.classList.add('svg-background');
+}
+
+
+
   
