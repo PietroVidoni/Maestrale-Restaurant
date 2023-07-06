0121d7084c19bbc9b212b3050a1b96c97968f3dd
PietroVidoni <pietro.vidoni04@gmail.com>
2023-07-06 18:24:53 +0200

    Fixed the animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,35 @@
 /* 
     Script.css
 */
+
+function toggleShape(link) {
+    var links = document.querySelectorAll('.navbar-item');
+
+    links.forEach(function (link) {
+        link.classList.remove('button-highlight');
+        link.style.color = 'var(--light-font-color)'
+    });
+
+    link.classList.add('button-highlight');
+    ink.style.color = 'var(--dark-font-color)'
+}
+
+const navbar = document.querySelector('.navbar');
+
+let prevScrollPos = window.pageYOffset;
+
+const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        navbar.classList.remove('navbar-hidden');
+        navbar.classList.add('navbar-visible');
+    } else {
+        navbar.classList.remove('navbar-visible');
+        navbar.classList.add('navbar-hidden');
+    }
+
+    prevScrollPos = currentScrollPos;
+};
+
+window.addEventListener('scroll', handleScroll);
