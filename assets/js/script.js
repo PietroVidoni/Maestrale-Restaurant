a221c59111c01510b8020e006367e8bf566ce3fd
PietroVidoni <pietro.vidoni04@gmail.com>
2023-07-07 23:06:56 +0200

    Bug fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,29 +33,26 @@ window.addEventListener('scroll', handleScroll);
 
 /* Navbar hamburger */
 const darkening = document.querySelectorAll('.darkening');
+const menu = document.querySelector('.navbar-burger');
+const button = document.querySelector('#burger-menu-button');
+const checkbox = document.getElementById('burger-menu-button');
+const burger = document.getElementById("burger-menu-button");
 
-document.addEventListener('click', function (e) {
-    var menu = document.querySelector('.navbar-burger');
-    var button = document.querySelector('#burger-menu-button');
+let menuState = false;
 
-    if (!menu.contains(e.target) && !button.contains(e.target)) {
-        var checkbox = document.getElementById('burger-menu-button');
-        checkbox.checked = false;
+burger.addEventListener('click', () => {
+    if (typeof CSS.supports('backdrop-filter', 'blur(0)')) {
+        menuState = menuState ? false : true;
 
-        darkening.forEach(function(item) {
-            toggleCSSProperty(item, 'filter', 'brightness(0)');
-        }); 
+        darkening.forEach(item => toggleCSSProperty(item, 'filter', 'brightness(0.4)'));
     }
 });
 
-const burger = document.getElementById("burger-menu-button");
+document.addEventListener('click', function (e) {
+    if (!menu.contains(e.target) && !button.contains(e.target) && menuState) {
+        checkbox.checked = false;
 
-burger.addEventListener('click', () => {
-
-    if (typeof navbarMenu.style.backdropFilter !== 'undefined') {
-        darkening.forEach(function(item) {
-            toggleCSSProperty(item, 'filter', 'brightness(0.4)');
-        });          
+        darkening.forEach(item => toggleCSSProperty(item, 'filter', 'brightness(1)'));
     }
 });
 
@@ -68,4 +65,3 @@ function toggleCSSProperty(element, property, value) {
         element.style.setProperty(property, value);
     }
 }
-
