7f593ab65acb7ddeef167fef982efc04f0334e0c
PietroVidoni <pietro.vidoni04@gmail.com>
2023-07-14 19:29:29 +0200

    Implemented php site

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+define('DB_HOST', 'localhost');       
+define('DB_NAME', 'maestralenewletter');   
+define('DB_USER', 'client');    
+define('DB_PASSWORD', 'client');  
+
+define('MAIL_HOST', 'smtp.gmail.com');
+define('MAIL_USERNAME', 'testprogrammi2023@gmail.com');
+define('MAIL_PASSWORD', 'yaupvvrydopqhcgf');
+define('MAIL_PORT', 587);
+?>
